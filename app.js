7623c63dbd878daec08c13cb62584aba560d9c87
Kevin <kevinrisky18@gmail.com>
2024-11-11 20:00:56 +0700

    add terbaru 3

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/", routes);
 
 app.get("/", (req, res) => {
-  res.send("aku di sini uhuy");
+  res.send("aku di sini uhuy 2");
 });
 
 app.listen(PORT, () => {
