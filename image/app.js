70f9b62548e4304ab89e11eb2f1497045016f9c2
Kevin <kevinrisky18@gmail.com>
2024-11-12 10:23:46 +0700

    add brok

--- image/app.js
+++ image/app.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/", routes);
 
 app.get("/", (req, res) => {
-  res.send("aku di sini uhuy 3");
+  res.send("aku di sini brok");
 });
 
 app.listen(PORT, () => {
